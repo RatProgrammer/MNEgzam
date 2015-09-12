84f013a9813f600ef425a3d65bda5af6be0d1e8f
Olo ;) <15olo1995@gmail.com>
2015-09-12 22:49:24 +0200

    Trzeba dokończy macierz odwrotna na podstawie LU

---
+++ MNEgzam/Matrix.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNEgzam
+{
+    internal class Matrix
+    {
+        private double[,] A;
+        private double[,] B;
+
+        public double[,] TransposeMatrix(double[,] A, int m, int n)
+        {
+            double[,] AT = new double[n, m];
+            for (int i = 0; i + 1 < m; i++)
+            {
+
+                for (int j = 0; j - 1 < n; j++)
+                {
+                    AT[i, j] = A[j, i];
+                    Console.WriteLine();
+                    Console.Write(AT[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+            return AT;
+        }
+
+        public double[,] MultiplyMatrix(double[,] A, double[,] B, int mA, int nA, int mB, int nB)
+        {
+
+            if (nA == mB)
+            {
+                double[,] C = new double[mA, nB];
+                for (int i = 0; i < mA; i++)
+                {
+                    for (int k = 0; k < nB; k++)
+                    {
+                        for (int j = 0; j < nA; j++)
+                        {
+                            C[i, k] += A[i, j]*B[j, k];
+
+
+                        }
+                        Console.WriteLine();
+                        Console.Write(C[i, k]);
+                    }
+                    Console.WriteLine();
+                }
+                return C;
+            }
+            return null;
+        }
+
+        public double DetCounter(double[,] A, int m, int n)
+        {
+            double[,] U;
+            double[,] L;
+            LuCompozition(A, n, m, out L, out U);
+            double det = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        det *= U[i, j];
+                    }
+                   
+                }
+            }
+            return det;
+        }
+
+
+        public void LuCompozition(double[,] A, int n, int m, out double[,] L, out double[,] U)
+        {
+            L = new double[n, n];
+            U = new double[n, n];
+            if (m == n)
+            {
+
+                double sumaU ;
+                double sumaL;
+               
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 1; j < n; j++)
+                    {
+                        L[i, i] = 1;
+                        U[0, i] = A[0, i];
+                        L[j, 0] = A[j, 0]/A[0, 0];
+                    }
+
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        
+                        sumaU = 0;
+                        if (i <= j)
+                        {
+                            for (int k = 0; k < i; k++)
+                            {
+
+                                sumaU += L[i, k]*U[k, j];
+
+                            }
+                            U[i, j] = A[i, j] - sumaU;
+                        }
+
+                        sumaL = 0;
+                        if (i < j)
+                        {
+                            for (int k = 0; k < i ; k++)
+                            {
+
+                                sumaL += L[j, k]*U[k, i];
+
+                            }
+                            L[j, i] = (A[j, i] - sumaL)/U[i, i];
+
+                        }
+
+
+
+                    }
+
+                }
+
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public double[,] InverseMatrix(double[,] A,int m, int n)
+        {
+            double[,] U;
+            double[,] L;
+            LuCompozition(A, n, m, out L, out U);
+            double[,] e=new double[n,n];
+            double [,] y=new double[n,n];
+            double [,] x=new double[n,n];
+            double sumaL = 0;
+            double sumaU = 0;
+            for (int i = 0; i < n; i++) // tworzenie macierzy jednostkowej e
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        e[i, j] = 1;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)//wyepłnienie macierzy y pierwszym wierszem mac e - zawsze!
+            {
+                y[0, i] = e[0, i];
+            }
+            for (int i = 1; i < n; i++)//wyznaczenie y
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        sumaL += L[i, k]*y[k, j];
+                    }
+                    y[i, j] = e[i, j] - sumaL;
+                    sumaL = 0;
+                }
+            }
+            for (int i = 0; i <n; i++)//wypełnienie ostatniego wiersza mac x
+            {
+                x[n-1, i] = y[n-1, i]/U[n-1, n-1];
+            }
+            for (int i = n-1; i <= n; i--)
+            {
+                if(i < 0)
+                    break;
+                for (int j = 1; j < n; j++)
+                {
+                    for (int k = i; k < n; k++)
+                    {
+                        sumaU += x[k, j]*U[i, k];
+                    }
+                    x[i, j] =(y[i, j] - sumaU)/U[i, i];
+                    sumaU = 0;
+                }
+            }
+
+
+            //for (int i = 2; i <= n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        for (int k = i-2; k < n-1; k++)
+            //        {
+            //            sumaU += x[k,j]*U[n-i,k];
+            //        }
+            //        x[n-i,j] = (y[n-i,j] - sumaU)/U[n-i, n-i];
+            //        sumaU = 0;
+            //    }
+            //}
+
+
+            return x;
+
+        }
+    }
+
+
+
+}
+
+    
+
---
+++ MNEgzam/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNEgzam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix=new Matrix();
+            double[,] A = new double[,] { { 3, 0.6,7.5,-3 }, {0.4, 0.5, 4,-8.5}, {0.3,-1,1,5.2}, {1,0.5,-0.1,0.5} };
+            double [,] B=new double[,] { {2,2},{1,4}};
+            int m = 4;
+            int n = 4;
+            //double[,] D=matrix.TransposeMatrix(A, m, n);
+            //double[,] C = matrix.MultiplyMatrix(A, B, 2, 2, 2, 2);
+
+            double[,] U;
+            double[,] L;
+            //matrix.LuCompozition(A, n, m, out L, out U);
+            //double det =matrix.DetCounter(A, m, n);
+           // Console.WriteLine(det);
+            double[,] C=matrix.InverseMatrix(A, m, n);
+           
+            Console.ReadKey();
+        }
+    }
+}
